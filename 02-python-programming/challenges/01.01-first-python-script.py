0af30ca63497ecf8571125e24dc2a4c2bc6889ff
Cyril Michino <cyrilmichino@gmail.com>
2023-03-05 13:29:55 +0300

    Intro to Python lesson

---
+++ 02-python-programming/challenges/01.01-first-python-script.py
@@ -0,0 +1,4 @@
+#Write your first Python script i.e. print Hello World
+## Edit the text below to make the code work in Pyton
+### Once done, add your repository to Github as your first script
+print Hello World
